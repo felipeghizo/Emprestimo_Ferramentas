faa2236fa78b2d78a037b9277dfb9601f7f76dff
m-effting <99153192+m-effting@users.noreply.github.com>
2024-05-01 09:58:06 -0300

    teste

---
+++ A3_programacao/src/Model/Amigo.java
@@ -0,0 +1,36 @@
+package Model;
+
+public class Amigo{
+
+    // Atributos
+    private String Nome;
+    private String Telefone;
+
+    // Construtor Default
+    public Amigo(){
+        this("", "");
+    }
+    // Construtor a
+    public Amigo(String Nome, String Telefone){
+        this.Nome = Nome;
+        this.Telefone = Telefone;
+    }
+
+    // Getters 
+    public String getNome(){
+        return this.Nome;
+    }
+    public String getTelefone(){
+        return this.Telefone;
+    }
+
+    // Setters
+    private void setNome(String nome){
+        this.Nome = nome;
+    }
+    private void setTelefone(String telefone){
+        this.Telefone = telefone;
+    }
+
+    // Metodos a serem implementados
+}
